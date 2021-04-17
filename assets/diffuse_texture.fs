82a566eb018aa708e6ee754db9ed7b4a75e95824
Tiaan Louw <tiaanl@gmail.com>
2021-04-17 13:44:41 +0200

    Working on model viewer.

--- assets/diffuse_texture.fs
+++ assets/diffuse_texture.fs
@@ -8,5 +8,6 @@ out vec4 final;
 uniform sampler2D uTexture;
 
 void main() {
-  final = texture(uTexture, vec2(vTexCoords.x, 1.0f - vTexCoords.y));
+  //final = texture(uTexture, vec2(vTexCoords.x, 1.0f - vTexCoords.y));
+  final = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
